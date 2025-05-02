2e196651eea793013d58c34db5b01b3f0624fde2
Eva Nunez <evahlivkova@MacBook-Pro-2.local>
2025-05-02 20:20:51 +0200

    Import findwinner and add functionality to identify winner of each game

--- hra.js
+++ hra.js
@@ -1,8 +1,21 @@
-console.log('cauky');
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
+
 let currentPlayer = 'circle';
 
-const gridButtons = document.querySelectorAll('.piskvorky__button-grid');
+const getGameStatus = () => {
+  const gameStatus = [];
+  const buttons = document.querySelectorAll('.piskvorky__button-grid');
+  buttons.forEach((button) => {
+    if (button.classList.contains('piskvorky__button-grid--circle')) {
+      gameStatus.push('o');
+    } else if (button.classList.contains('piskvorky__button-grid--cross')) {
+      gameStatus.push('x');
+    } else gameStatus.push('_');
+  });
+  return gameStatus;
+};
 
+const gridButtons = document.querySelectorAll('.piskvorky__button-grid');
 gridButtons.forEach((button) => {
   button.addEventListener('click', (event) => {
     if (currentPlayer === 'circle') {
@@ -11,6 +24,22 @@ gridButtons.forEach((button) => {
       event.target.classList.add('piskvorky__button-grid--cross');
     }
     event.target.disabled = true;
+
+    const gameStatus = getGameStatus();
+    const winner = findWinner(gameStatus);
+
+    if (winner === 'o' || winner === 'x') {
+      const winnerSymbol = winner === 'o' ? 'kolečko' : 'křížek';
+      if ((winnerSymbol === 'tie')) {
+        alert('hra skončila remízou');
+      }
+      setTimeout(() => {
+        alert(`Vyhrál hráč se symbolem ${winnerSymbol}`);
+        location.reload();
+        return;
+      }, 1000);
+    }
+
     const currentPlayerIcon = document.querySelector('#playerIndicator');
     currentPlayerIcon.classList.remove('piskorky__button--player--circle');
     currentPlayerIcon.classList.remove('piskorky__button--player--cross');
@@ -27,13 +56,13 @@ gridButtons.forEach((button) => {
 
 const restartButtonElement = document.querySelector('#restart-button');
 restartButtonElement.addEventListener('click', (event) => {
-  const confirmValue = confirm('Opravdu chces zacit znovu?')
+  const confirmValue = confirm('Opravdu chces zacit znovu?');
   if (confirmValue === false) {
     event.preventDefault();
   }
 });
 
 /* 
-Může se stát, že uživatel se omylem uklikne a modrým tlačítkem pro restart přijde o rozehranou hru. Proto přidej modrému odkazu posluchač události, který se po kliknutí uživatele zeptá zabudovanou funcí confirm, jestli chce hru opravdu restartovat. Pokud ne, zavolej event.preventDefault(), čímž zabráníš tomu, aby prohlížeč přešel na odkazovanou stránku (tj. načetl stránku s prázdnou hrou). Funkce confirm vrací true nebo false podle toho, zda uživatel souhlasil nebo nesouhlasil s potvrzovací zprávou v dialogu.
+1. funkce, ktera vytvori pole pro importovanou funkci findWinner
 
 */
