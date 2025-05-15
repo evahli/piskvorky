e3f5767f57f5247f29be2c370c2f8c42e6b34abe
Eva Nunez <evahlivkova@MacBook-Pro-2.local>
2025-05-15 21:03:45 +0200

    refactored the game event listener - moved the function to switch player outside of the event listener, wrote makeAiMove function to get the x player move from AI, added let gameEnded to make sure the API is not being called after there is a winner, all buttons are set to disable while we wait for the API response, after the AI click is done all empty buttons become clickable again.

--- hra.js
+++ hra.js
@@ -16,6 +16,66 @@ const getGameStatus = () => {
   return gameStatus;
 };
 
+// funkce pro zmenu hrace
+const switchCurrentPlayer = () => {
+  const currentPlayerIcon = document.querySelector('#playerIndicator');
+  currentPlayerIcon.classList.remove('piskorky__button--player--circle');
+  currentPlayerIcon.classList.remove('piskorky__button--player--cross');
+  currentPlayerIcon.classList.remove('piskorky__button--player');
+  if (currentPlayer === 'circle') {
+    currentPlayer = 'cross';
+    currentPlayerIcon.classList.add('piskorky__button--player--cross');
+  } else {
+    currentPlayer = 'circle';
+    currentPlayerIcon.classList.add('piskorky__button--player--circle');
+  }
+};
+
+// funkce pro pridani AI tahu misto krizku:
+
+const makeAiMove = async () => {
+  const gameStatus = getGameStatus(); 
+  const gridButtons = document.querySelectorAll('.piskvorky__button-grid');
+  // disable vsechny tlacitka, aby nikdo nemohl kliknout, kdyz se ceka na API response
+  gridButtons.forEach((button) => {
+    button.disabled = true;
+  });
+  // volani API se tahem od AI
+  const response = await fetch(
+    'https://piskvorky.czechitas-podklady.cz/api/suggest-next-move',
+    {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        board: gameStatus,
+        player: 'x',
+      }),
+    },
+  );
+  // zpracovani odpovedi od AI: 
+  const data = await response.json();
+  const x = data.position.x;
+  const y = data.position.y;
+  const index = x + y * 10;
+  // kliknuti na tlacitko poslane od AI:
+  const buttons = document.querySelectorAll('.piskvorky__button-grid');
+  buttons[index].disabled = false; 
+  buttons[index].click();
+  
+  gridButtons.forEach((button) => {
+    if (
+      !button.classList.contains('piskvorky__button-grid--circle') &&
+      !button.classList.contains('piskvorky__button-grid--cross')
+    ) {
+      button.disabled = false;
+    }
+  });
+
+};
+
+// hra - co se stane pri kliknuti
 const gridButtons = document.querySelectorAll('.piskvorky__button-grid');
 gridButtons.forEach((button) => {
   button.addEventListener('click', (event) => {
@@ -25,34 +85,36 @@ gridButtons.forEach((button) => {
       event.target.classList.add('piskvorky__button-grid--cross');
     }
     event.target.disabled = true;
+
     // cast funkce pro kontrolu viteze hry
     const gameStatus = getGameStatus();
     const winner = findWinner(gameStatus);
 
+    let gameEnded = false;
+
     if (winner === 'o' || winner === 'x') {
       const winnerSymbol = winner === 'o' ? 'kolečko' : 'křížek';
-      if ((winnerSymbol === 'tie')) {
-        alert('hra skončila remízou');
-        location.reload();
-        return;
-      }
+      gameEnded = true;
       setTimeout(() => {
         alert(`Vyhrál hráč se symbolem ${winnerSymbol}`);
         location.reload();
         return;
       }, 400);
+    } else if (winner === 'tie') {
+      gameEnded = true;
+      setTimeout(() => {
+        alert('Hra skončila remízou');
+        location.reload();
+        return;
+      }, 400);
     }
 
-    const currentPlayerIcon = document.querySelector('#playerIndicator');
-    currentPlayerIcon.classList.remove('piskorky__button--player--circle');
-    currentPlayerIcon.classList.remove('piskorky__button--player--cross');
-    currentPlayerIcon.classList.remove('piskorky__button--player');
-    if (currentPlayer === 'circle') {
-      currentPlayer = 'cross';
-      currentPlayerIcon.classList.add('piskorky__button--player--cross');
-    } else {
-      currentPlayer = 'circle';
-      currentPlayerIcon.classList.add('piskorky__button--player--circle');
+    // pokud hra jeste neskoncila, switch players; pokud je na rade x, hraje AI
+    if (!gameEnded) {
+      switchCurrentPlayer();
+      if (currentPlayer === 'cross') {
+        makeAiMove();
+      }
     }
   });
 });
@@ -64,4 +126,3 @@ restartButtonElement.addEventListener('click', (event) => {
     event.preventDefault();
   }
 });
-
